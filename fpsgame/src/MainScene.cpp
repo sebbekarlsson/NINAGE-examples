45c4240a7cacfd86c8c8dbc86f1f9f71fdbb751c
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-07-24 14:33:08 +0200

    WASD movement

--- fpsgame/src/MainScene.cpp
+++ fpsgame/src/MainScene.cpp
@@ -12,6 +12,9 @@ MainScene::MainScene(): Scene() {
 }
 
 void MainScene::init(float delta) {
+    SDL_ShowCursor(SDL_DISABLE);
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+
     for (int xx = 0; xx < 16; xx++) {
         for (int yy = -20; yy < 16; yy++) {
             Ground* ground = new Ground(xx * 2, -2.2f, yy * 2);
@@ -22,6 +25,26 @@ void MainScene::init(float delta) {
 
 void MainScene::tick(float delta) {
     this->tickDefault(delta);
+
+    if (app->keyboardDown(SDL_SCANCODE_W)) {
+        this->camera->position->x += sin(EngineMath::toRadians(this->camera->yrotation)) * 0.08f;
+        this->camera->position->z -= cos(EngineMath::toRadians(this->camera->yrotation)) * 0.08f;
+    }
+
+    if (app->keyboardDown(SDL_SCANCODE_S)) {
+        this->camera->position->x += sin(EngineMath::toRadians(this->camera->yrotation - 180.0f)) * 0.08f;
+        this->camera->position->z -= cos(EngineMath::toRadians(this->camera->yrotation - 180.0f)) * 0.08f;
+    }
+
+    if (app->keyboardDown(SDL_SCANCODE_A)) {
+        this->camera->position->x += sin(EngineMath::toRadians(this->camera->yrotation - 90.0f)) * 0.08f;
+        this->camera->position->z -= cos(EngineMath::toRadians(this->camera->yrotation - 90.0f)) * 0.08f;
+    }
+
+    if (app->keyboardDown(SDL_SCANCODE_D)) {
+        this->camera->position->x += sin(EngineMath::toRadians(this->camera->yrotation + 90.0f)) * 0.08f;
+        this->camera->position->z -= cos(EngineMath::toRadians(this->camera->yrotation + 90.0f)) * 0.08f;
+    }
 }
 
 void MainScene::draw(float delta) {

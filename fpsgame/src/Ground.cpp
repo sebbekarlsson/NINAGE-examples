89d869e43cc3d92c7a6ffe2aa72462dcdfca5cfe
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-07-27 13:24:38 +0200

    better collisions

--- fpsgame/src/Ground.cpp
+++ fpsgame/src/Ground.cpp
@@ -27,40 +27,40 @@ void Ground::draw(float delta) {
     glBegin(GL_QUADS);
     // Front Face
     glNormal3f(0.0f, 0.0f, 1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Left
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Right
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f,  1.0f);	// Bottom Left
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 0.0f,  1.0f);	// Bottom Right
     glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Right
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Left
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f,  1.0f,  1.0f);	// Top Left
     // Back Face
-    glNormal3f(0.0f, 0.0f, -1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Right
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Right
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Left
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Left
+    glNormal3f(0.0f, 0.0f, 0.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);	// Bottom Right
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(0.0f,  1.0f, 0.0f);	// Top Right
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, 0.0f);	// Top Left
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, 0.0f, 0.0f);	// Bottom Left
     // Top Face
     glNormal3f(0.0f, 1.0f, 0.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Bottom Left
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f,  1.0f, 0.0f);	// Top Left
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f,  1.0f,  1.0f);	// Bottom Left
     glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Bottom Right
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, 0.0f);	// Top Right
     // Bottom Face
-    glNormal3f(0.0f, -1.0f, 0.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Right
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Top Left
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right
+    glNormal3f(0.0f, 0.0f, 0.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(0.0f, 0.0f, 0.0f);	// Top Right
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, 0.0f, 0.0f);	// Top Left
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, 0.0f,  1.0f);	// Bottom Left
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, 0.0f,  1.0f);	// Bottom Right
     // Right face
     glNormal3f(1.0f, 0.0f, 0.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Right
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 0.0f, 0.0f);	// Bottom Right
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, 0.0f);	// Top Right
     glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Left
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, 0.0f,  1.0f);	// Bottom Left
     // Left Face
-    glNormal3f(-1.0f, 0.0f, 0.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Right
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left
+    glNormal3f(0.0f, 0.0f, 0.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);	// Bottom Left
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, 0.0f,  1.0f);	// Bottom Right
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(0.0f,  1.0f,  1.0f);	// Top Right
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f,  1.0f, 0.0f);	// Top Left
     glEnd();
 
     glPopMatrix();
--- fpsgame/src/MainScene.cpp
+++ fpsgame/src/MainScene.cpp
@@ -3,11 +3,19 @@
 #include <random>
 
 
-float xRotated = 0.5f;
-float yRotated = 1.0f;
-float zRotated = 2.5f;
 bool onGround = false;
-float py = 0.0f;
+
+float camprevx = 0.0f;
+float camprevy = 0.0f;
+float camprevz = 0.0f;
+
+float distance_to_x0 = 0;
+float distance_to_x1 = 0;
+
+float distance_to_z0 = 0;
+float distance_to_z1 = 0;
+
+float distance_to_y1 = 0;
 
 MainScene::MainScene(): Scene() {
 }
@@ -17,25 +25,50 @@ void MainScene::init(float delta) {
     SDL_SetRelativeMouseMode(SDL_TRUE);
 
     camera->yfriction = 0.0001f;
+    camera->position->y = 30.0f;
 
     for (int xx = 0; xx < 16; xx++) {
-        for (int yy = -20; yy < 16; yy++) {
-            Ground* ground = new Ground(xx * 2, -2.2f, yy * 2);
+        for (int zz = 0; zz < 16; zz++) {
+            Ground* ground = new Ground(xx, -3.0f, zz);
             this->instantiate(ground);
         }
     }
 
-    Ground* ground = new Ground(4.0f, (-2.2f) + 2, 4.0f);
+    Ground* ground = new Ground(4.0f, -2.0f, 4.0f);
     this->instantiate(ground);
+
+    Ground* ground0 = new Ground(4.0f, -1.0f, 4.0f);
+    this->instantiate(ground0);
+
+    Ground* ground1 = new Ground(4.0f, 0.0f, 4.0f);
+    this->instantiate(ground1);
+
+
+    Ground* ground2 = new Ground(6.0f, -2.0f, 4.0f);
+    this->instantiate(ground2);
+
+    Ground* ground3 = new Ground(6.0f, -1.0f, 4.0f);
+    this->instantiate(ground3);
+
+    Ground* ground4 = new Ground(6.0f, 0.0f, 4.0f);
+    this->instantiate(ground4);
 }
 
 void MainScene::tick(float delta) {
     this->tickDefault(delta);
+    onGround = false;
+
     camera->updatePhysics(delta);
 
-    onGround = false;
-    py = 0.0f;
-    
+    camprevx = camera->position->x;
+    camprevz = camera->position->z;
+    camprevy = camera->position->y;
+
+    distance_to_x0 = 0.0f;
+    distance_to_x1 = 0.0f;
+    distance_to_y1 = 0.0f;
+    distance_to_z0 = 0.0f;
+    distance_to_z1 = 0.0f;
 
     if (app->keyboardDown(SDL_SCANCODE_W)) {
         this->camera->position->x += sin(EngineMath::toRadians(this->camera->yrotation)) * 0.08f;
@@ -62,20 +95,45 @@ void MainScene::tick(float delta) {
         //if ((*it) == (*it2) || (*it)->interactive == false) { ++it2; continue; }
 
         //(*it2)->scene(delta, (*it));
-        
+
         if (camera->intersectsWith(delta, (Entity*)(*it2))) {
             onGround = true;
-            py = ((*it2)->getY() + (*it2)->collisionBox->height + camera->collisionBox->height);
+
+            if (camera->position->y - camera->collisionBox->height < (*it2)->position->y) {
+                distance_to_x0 = std::max((camprevx + camera->collisionBox->width), (*it2)->position->x) - std::min((camprevx + camera->collisionBox->width), (*it2)->position->x);
+                distance_to_x1 = std::max((*it2)->position->x + (*it2)->collisionBox->width, camprevx) - std::min((*it2)->position->x + (*it2)->collisionBox->width, camprevx);
+
+                distance_to_z0 = std::max((camprevz + camera->collisionBox->depth), (*it2)->position->z) - std::min((camprevz + camera->collisionBox->depth), (*it2)->position->z);
+                distance_to_z1 = std::max((*it2)->position->z + (*it2)->collisionBox->depth, camprevz) - std::min((*it2)->position->z + (*it2)->collisionBox->depth, camprevz);
+
+            }
+
+            if (camprevy > (*it2)->position->y - (*it2)->collisionBox->height) {
+                distance_to_y1 = std::max((*it2)->position->y - (*it2)->collisionBox->height, camprevy) - std::min((*it2)->position->y - (*it2)->collisionBox->height, camprevy);
+            }
         }
 
         ++it2;
+    }
+
+    if (distance_to_x0 > 0.0f || distance_to_x1 > 0.0f) {
+        camera->position->x = camprevx;
+    }
+
+    if (distance_to_z0 > 0.0f || distance_to_z1 > 0.0f) {
+        camera->position->z = camprevz;
+    }
+
+    if (distance_to_y1 <= 0.0f) {
+        onGround = false;
+        camera->position->y = camprevy;
     }
 
     if (!onGround) {
         camera->dy -= 0.0007f;
     } else {
         camera->dy = 0.0f;
-        camera->position->y = py;
+        camera->position->y = camprevy;
     }
 
     if (app->keyboardDown(SDL_SCANCODE_SPACE) && onGround) {
@@ -88,11 +146,11 @@ void MainScene::draw(float delta) {
 }
 
 void MainScene::mouseMoveEvent(
-    int &mouseX,
-    int &mouseY,
-    int &deltaMouseX,
-    int &deltaMouseY
-) {
+        int &mouseX,
+        int &mouseY,
+        int &deltaMouseX,
+        int &deltaMouseY
+        ) {
     this->camera->xrotation += (deltaMouseY * 0.25f);
     this->camera->yrotation += (deltaMouseX * 0.25f);
 }
